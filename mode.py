89921b2481a1d5a5d8bf05c8f3015e09a669471f
ZOIjimmy <b09902037@ntu.edu.tw>
2023-10-26 00:01:56 +0800

    mode detection

--- mode.py
+++ mode.py
@@ -2,11 +2,18 @@ import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import os
+import random
 
+directory = "../../../wav"
+files = [f for f in os.listdir(directory) if f.endswith(".wav")]
+random_file = random.choice(files)
+random_path = os.path.join(directory, random_file)
+print(random_path)
 
-y, sr = librosa.load("psy.wav")
-# y_harmonic, y_percussive = librosa.effects.hpss(y)
-y_harmonic, y_percussive = librosa.effects.hpss(y[2006550:2271150])
+y, sr = librosa.load(random_path)
+y_harmonic, y_percussive = librosa.effects.hpss(y)
+# y_harmonic, y_percussive = librosa.effects.hpss(y[2006550:2271150])
 y = y_harmonic
 stft = librosa.stft(y)
 stft[:17,:] = np.zeros((17,stft.shape[-1]))
@@ -16,6 +23,26 @@ f0, voiced_flag, voiced_probs = librosa.pyin(y,
                                              fmax=librosa.note_to_hz('C7'))
 times = librosa.times_like(f0)
 
+note = "A Bb B C C# D Eb E F F# G Ab".split()
+plot = [0] * 12
+last = float("nan")
+for i in range(len(f0)):
+    if not np.isnan(f0[i]):
+        f0[i] = 2**(round(math.log2(f0[i]/220)*12)/12)*220
+        pitch = round(math.log2(f0[i]/220)*12)%12
+        if pitch == last:
+            plot[pitch] += 1
+        last = pitch
+    else:
+        last = float("nan")
+possibility = np.zeros(12)
+for j in range(12):
+    possibility[j] = plot[j] // 2
+    for i in (0,2,4,5,7,9,11):
+        possibility[j] += plot[(i+j)%12]
+print(plot)
+print(possibility)
+print(note[np.argmax(possibility)])
 
 D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
 fig, ax = plt.subplots()
